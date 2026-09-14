4ce9dfe374de11bce702573164196a97195e2c95
HaiderShafiq <103728525+HaiderShafiq@users.noreply.github.com>
2026-09-14 16:58:48 -0400

    Update hw4-multiple-greetings.py

--- hw4-multiple-greetings.py
+++ hw4-multiple-greetings.py
@@ -1,3 +1,9 @@
+#Email: haider.shafiq72@login.cuny.edu/ haidershafiqq@gmail.com
+#Name: Shafiq Haider
+
+print("Email: haider.shafiq72@login.cuny.edu")
+print("Name: Shafiq Haider")
+
 empl_id = "72"  
 times = int(empl_id) + 10
 
